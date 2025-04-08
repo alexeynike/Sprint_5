f583295b0eec406a40d47f6b238d3856cb7a5b70
Alex Nikolaev <nuclearcatt@gmail.com>
2025-04-08 23:03:53 +0700

    add gitignore

--- tests/test_login.py
+++ tests/test_login.py
@@ -7,77 +7,75 @@ from test_data import *
 class TestLogin:
     def test_login_from_main_page(self, driver):
         driver.get(main_url)
-        driver.find_element(By.XPATH, HomePageLocators.login_btn).click()
-        driver.find_element(By.XPATH, RegistrationPageLocators.email_field).send_keys(login)
-        driver.find_element(By.XPATH, RegistrationPageLocators.password_field).send_keys(password)
-        driver.find_element(By.XPATH, LoginPageLocators.login_btn).click()
-        assert driver.find_element(By.XPATH, HomePageLocators.order_btn).is_displayed()
+        driver.find_element(By.XPATH, HomePageLocators.LOGIN_BTN).click()
+        driver.find_element(By.XPATH, RegistrationPageLocators.EMAIL_FIELD).send_keys(login)
+        driver.find_element(By.XPATH, RegistrationPageLocators.PASSWORD_FIELD).send_keys(password)
+        driver.find_element(By.XPATH, LoginPageLocators.LOGIN_BTN).click()
+        assert driver.find_element(By.XPATH, HomePageLocators.ORDER_BTN).is_displayed()
 
     def test_login_from_profile_page(self, driver):
         driver.get(main_url)
-        driver.find_element(By.XPATH, HomePageLocators.profile_link).click()
-        driver.find_element(By.XPATH, RegistrationPageLocators.email_field).send_keys(login)
-        driver.find_element(By.XPATH, RegistrationPageLocators.password_field).send_keys(password)
-        driver.find_element(By.XPATH, LoginPageLocators.login_btn).click()
-        assert driver.find_element(By.XPATH, HomePageLocators.order_btn).is_displayed()
+        driver.find_element(By.XPATH, HomePageLocators.PROFILE_LINK).click()
+        driver.find_element(By.XPATH, RegistrationPageLocators.EMAIL_FIELD).send_keys(login)
+        driver.find_element(By.XPATH, RegistrationPageLocators.PASSWORD_FIELD).send_keys(password)
+        driver.find_element(By.XPATH, LoginPageLocators.LOGIN_BTN).click()
+        assert driver.find_element(By.XPATH, HomePageLocators.ORDER_BTN).is_displayed()
 
     def test_login_from_registration_page(self, driver):
         driver.get(main_url + '/register')
-        driver.find_element(By.XPATH, RegistrationPageLocators.login_link).click()
-        driver.find_element(By.XPATH, RegistrationPageLocators.email_field).send_keys(login)
-        driver.find_element(By.XPATH, RegistrationPageLocators.password_field).send_keys(password)
-        driver.find_element(By.XPATH, LoginPageLocators.login_btn).click()
-        assert driver.find_element(By.XPATH, HomePageLocators.order_btn).is_displayed()
+        driver.find_element(By.XPATH, RegistrationPageLocators.LOGIN_LINK).click()
+        driver.find_element(By.XPATH, RegistrationPageLocators.EMAIL_FIELD).send_keys(login)
+        driver.find_element(By.XPATH, RegistrationPageLocators.PASSWORD_FIELD).send_keys(password)
+        driver.find_element(By.XPATH, LoginPageLocators.LOGIN_BTN).click()
+        assert driver.find_element(By.XPATH, HomePageLocators.ORDER_BTN).is_displayed()
 
     def test_login_from_restore_page(self, driver):
         driver.get(main_url + '/forgot-password')
-        driver.find_element(By.XPATH, RestorePageLocators.login_link).click()
-        driver.find_element(By.XPATH, RegistrationPageLocators.email_field).send_keys(login)
-        driver.find_element(By.XPATH, RegistrationPageLocators.password_field).send_keys(password)
-        driver.find_element(By.XPATH, LoginPageLocators.login_btn).click()
-        assert driver.find_element(By.XPATH, HomePageLocators.order_btn).is_displayed()
+        driver.find_element(By.XPATH, RestorePageLocators.LOGIN_LINK).click()
+        driver.find_element(By.XPATH, RegistrationPageLocators.EMAIL_FIELD).send_keys(login)
+        driver.find_element(By.XPATH, RegistrationPageLocators.PASSWORD_FIELD).send_keys(password)
+        driver.find_element(By.XPATH, LoginPageLocators.LOGIN_BTN).click()
+        assert driver.find_element(By.XPATH, HomePageLocators.ORDER_BTN).is_displayed()
 
     def test_open_profile_page(self, driver):
         driver.get(main_url)
-        driver.find_element(By.XPATH, HomePageLocators.login_btn).click()
-        driver.find_element(By.XPATH, RegistrationPageLocators.email_field).send_keys(login)
-        driver.find_element(By.XPATH, RegistrationPageLocators.password_field).send_keys(password)
-        driver.find_element(By.XPATH, LoginPageLocators.login_btn).click()
-        driver.find_element(By.XPATH, HomePageLocators.profile_link).click()
+        driver.find_element(By.XPATH, HomePageLocators.LOGIN_BTN).click()
+        driver.find_element(By.XPATH, RegistrationPageLocators.EMAIL_FIELD).send_keys(login)
+        driver.find_element(By.XPATH, RegistrationPageLocators.PASSWORD_FIELD).send_keys(password)
+        driver.find_element(By.XPATH, LoginPageLocators.LOGIN_BTN).click()
+        driver.find_element(By.XPATH, HomePageLocators.PROFILE_LINK).click()
         assert WebDriverWait(driver, 3).until(EC.url_contains('/account/profile'))
-
 
     def test_exit_from_login_page(self, driver):
         driver.get(main_url)
-        driver.find_element(By.XPATH, HomePageLocators.login_btn).click()
-        driver.find_element(By.XPATH, RegistrationPageLocators.email_field).send_keys(login)
-        driver.find_element(By.XPATH, RegistrationPageLocators.password_field).send_keys(password)
-        driver.find_element(By.XPATH, LoginPageLocators.login_btn).click()
-        driver.find_element(By.XPATH, HomePageLocators.profile_link).click()
-        driver.find_element(By.XPATH, ProfilePageLocators.logout_btn).click()
+        driver.find_element(By.XPATH, HomePageLocators.LOGIN_BTN).click()
+        driver.find_element(By.XPATH, RegistrationPageLocators.EMAIL_FIELD).send_keys(login)
+        driver.find_element(By.XPATH, RegistrationPageLocators.PASSWORD_FIELD).send_keys(password)
+        driver.find_element(By.XPATH, LoginPageLocators.LOGIN_BTN).click()
+        driver.find_element(By.XPATH, HomePageLocators.PROFILE_LINK).click()
+        driver.find_element(By.XPATH, ProfilePageLocators.LOGOUT_BTN).click()
         assert WebDriverWait(driver, 3).until(EC.url_contains('/login'))
 
     def test_constructor_tab_default_active(self, driver):
         driver.get(main_url)
-        active_tab = driver.find_element(By.XPATH, HomePageLocators.bread_tab)
+        active_tab = driver.find_element(By.XPATH, HomePageLocators.BREAD_TAB)
         assert active_class in active_tab.get_attribute('class').split()
 
     def test_move_to_sauce_tab(self, driver):
         driver.get(main_url)
-        sauce_tab = driver.find_element(By.XPATH, HomePageLocators.sauce_tab)
+        sauce_tab = driver.find_element(By.XPATH, HomePageLocators.SAUCE_TAB)
         sauce_tab.click()
         assert active_class in sauce_tab.get_attribute('class').split()
 
     def test_move_to_ingredients_tab(self, driver):
         driver.get(main_url)
-        ingredients_tab = driver.find_element(By.XPATH, HomePageLocators.ingredients_tab)
+        ingredients_tab = driver.find_element(By.XPATH, HomePageLocators.INGREDIENTS_TAB)
         ingredients_tab.click()
         assert active_class in ingredients_tab.get_attribute('class').split()
 
     def test_move_from_ingredients_tab(self, driver):
         driver.get(main_url)
-        driver.find_element(By.XPATH, HomePageLocators.ingredients_tab).click()
-        bread_tab = driver.find_element(By.XPATH, HomePageLocators.bread_tab)
+        driver.find_element(By.XPATH, HomePageLocators.INGREDIENTS_TAB).click()
+        bread_tab = driver.find_element(By.XPATH, HomePageLocators.BREAD_TAB)
         bread_tab.click()
         assert active_class in bread_tab.get_attribute('class').split()
-
